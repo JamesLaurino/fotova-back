0a0f025f0e06cdebcab1d38e4c8902d2c41451fe
jamesLaurino <petersthomas90@gmail.com>
2025-03-08 14:47:38 +0100

    FTO-0056 login demo controller

---
+++ src/main/java/com/fotova/dto/authentification/ResponseDto.java
@@ -0,0 +1,14 @@
+package com.fotova.dto.authentification;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDto {
+    private boolean isAuthenticate;
+}
