6a26c78dd9c9aaae37de03d92efb60424c852cc5
jamesLaurino <petersthomas90@gmail.com>
2025-03-11 16:48:56 +0100

    FTO-0043 implement reset password

---
+++ src/main/java/com/fotova/dto/request/ResetPasswordRequest.java
@@ -0,0 +1,13 @@
+package com.fotova.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class ResetPasswordRequest {
+    private String email;
+    private String newPassword;
+}
