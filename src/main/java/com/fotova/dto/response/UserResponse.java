7ef716e5a4b436204463cb2d7938c6bc9989522f
jamesLaurino <petersthomas90@gmail.com>
2025-07-21 16:29:34 +0200

    refactoring

--- src/main/java/com/fotova/dto/response/UserResponse.java
+++ src/main/java/com/fotova/dto/response/UserResponse.java
@@ -1,6 +1,7 @@
 package com.fotova.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fotova.dto.client.ClientAddressDto;
 import com.fotova.entity.ERole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +19,7 @@ public class UserResponse {
     private Integer id;
     private String username;
     private String email;
+    private ClientAddressDto address;
 
     @JsonProperty("is_verified")
     private Boolean isActive;
