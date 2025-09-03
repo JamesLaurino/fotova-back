8a2564c29e52041e37a616cbe3fbf417f40bb8bd
jamesLaurino <petersthomas90@gmail.com>
2025-09-03 13:22:19 +0200

    FTO-117 implement swagger doc - client.

--- src/main/java/com/fotova/service/client/ClientService.java
+++ src/main/java/com/fotova/service/client/ClientService.java
@@ -6,6 +6,7 @@ import com.fotova.dto.comment.CommentDto;
 import com.fotova.entity.AddressEntity;
 import com.fotova.entity.ClientEntity;
 import com.fotova.entity.CommentEntity;
+import com.fotova.exception.NotFoundException;
 import com.fotova.repository.address.AddressRepositoryImpl;
 import com.fotova.repository.client.ClientRepositoryImpl;
 import com.fotova.repository.comment.CommentRepositoryImpl;
@@ -43,50 +44,66 @@ public class ClientService {
 
     public ClientDto getClientById(Integer id) {
         ClientEntity clientEntity = clientRepositoryImpl.findById(id);
+        if (clientEntity == null) {
+            throw new NotFoundException("Client not found for id: " + id);
+        }
         return clientMapper.mapClientToClientDto(clientEntity);
     }
 
     public ClientDto updateAddressClient(Integer clientId,AddressDto addressDto) {
+        try {
+            ClientEntity clientEntity = clientRepositoryImpl.findById(clientId);
 
-        ClientEntity clientEntity = clientRepositoryImpl.findById(clientId);
-
-        if(clientEntity !=null && clientEntity.getAddress() != null){
-            //TODO retirer le mapAddressToAddressEntity pour le addressMapper.mapToAddressEntity
-            AddressEntity addressEntity = clientMapper.mapAddressToAddressEntity(addressDto);
-            addressEntity.setId(clientEntity.getAddress().getId());
-            addressRepositoryImpl.save(addressEntity);
-            return clientMapper.mapClientToClientDto(clientRepositoryImpl.findById(clientId));
+            if(clientEntity !=null && clientEntity.getAddress() != null){
+                AddressEntity addressEntity = clientMapper.mapAddressToAddressEntity(addressDto);
+                addressEntity.setId(clientEntity.getAddress().getId());
+                addressRepositoryImpl.save(addressEntity);
+                return clientMapper.mapClientToClientDto(clientRepositoryImpl.findById(clientId));
+            }
+        } catch (Exception e) {
+            throw new NotFoundException("Client not found for id: " + clientId);
         }
 
         return null;
     }
 
     public ClientDto addAddressClient(Integer clientId,AddressDto addressDto) {
+        try {
+            AddressEntity addressEntity = addressMapper.mapToAddressEntity(addressDto);
+            addressEntity = addressRepositoryImpl.save(addressEntity);
 
-        AddressEntity addressEntity = addressMapper.mapToAddressEntity(addressDto);
-        addressEntity = addressRepositoryImpl.save(addressEntity);
+            ClientEntity clientEntity = clientRepositoryImpl.findById(clientId);
+            clientEntity.setAddress(addressEntity);
+            clientRepositoryImpl.save(clientEntity);
 
-        ClientEntity clientEntity = clientRepositoryImpl.findById(clientId);
-        clientEntity.setAddress(addressEntity);
-        clientRepositoryImpl.save(clientEntity);
-
-        return clientMapper.mapClientToClientDto(clientRepositoryImpl.findById(clientId));
+            return clientMapper.mapClientToClientDto(clientRepositoryImpl.findById(clientId));
+        } catch (Exception e) {
+            throw new NotFoundException("Client not found for id: " + clientId);
+        }
     }
 
     public String addCommentClient(Integer clientId, CommentDto comment) {
-        CommentEntity commentEntity = commentMapper.mapToCommentEntity(comment);
-        commentEntity = commentRepositoryImpl.save(commentEntity);
-        commentRepositoryImpl.setCommentClientId(clientId, commentEntity.getId());
-        return "comment added successfully to client : " +  clientId;
+        try {
+            CommentEntity commentEntity = commentMapper.mapToCommentEntity(comment);
+            commentEntity = commentRepositoryImpl.save(commentEntity);
+            commentRepositoryImpl.setCommentClientId(clientId, commentEntity.getId());
+            return "comment added successfully to client : " +  clientId;
+        } catch (Exception e) {
+            throw new NotFoundException("Client not found for id: " + clientId);
+        }
     }
 
     public String deleteClientById(Integer clientId) {
-        ClientEntity clientEntity = clientRepositoryImpl.clientAnonymization(clientId);
-        if(clientEntity != null) {
-            clientEntity = clientMapper.anonymization(clientEntity);
-            clientRepositoryImpl.save(clientEntity);
-            clientRepositoryImpl.updateClientAddress(clientEntity.getId());
-            return "client deleted successfully : " +  clientId;
+        try {
+            ClientEntity clientEntity = clientRepositoryImpl.clientAnonymization(clientId);
+            if(clientEntity != null) {
+                clientEntity = clientMapper.anonymization(clientEntity);
+                clientRepositoryImpl.save(clientEntity);
+                clientRepositoryImpl.updateClientAddress(clientEntity.getId());
+                return "client deleted successfully : " +  clientId;
+            }
+        } catch (Exception e) {
+            throw new NotFoundException("Client not found for id: " + clientId);
         }
         return null;
     }
