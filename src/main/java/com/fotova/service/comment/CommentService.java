79ecf46d8f18c348cdd31e3751d28ec3c6e85507
jamesLaurino <petersthomas90@gmail.com>
2025-09-03 13:14:30 +0200

    FTO-117 implement swagger doc - comment.

--- src/main/java/com/fotova/service/comment/CommentService.java
+++ src/main/java/com/fotova/service/comment/CommentService.java
@@ -2,6 +2,8 @@ package com.fotova.service.comment;
 
 import com.fotova.dto.comment.CommentDto;
 import com.fotova.entity.CommentEntity;
+import com.fotova.exception.DataExistException;
+import com.fotova.exception.NotFoundException;
 import com.fotova.repository.comment.CommentRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,24 +24,39 @@ public class CommentService {
     }
 
     public CommentDto saveComment(CommentDto commentDto) {
-        CommentEntity commentEntity = commentMapper.mapToCommentEntity(commentDto);
-        commentEntity = commentRepositoryImpl.save(commentEntity);
-        return commentMapper.mapToCommentDto(commentEntity);
+        try {
+            CommentEntity commentEntity = commentMapper.mapToCommentEntity(commentDto);
+            commentEntity = commentRepositoryImpl.save(commentEntity);
+            return commentMapper.mapToCommentDto(commentEntity);
+        } catch (Exception e) {
+            throw new DataExistException("Comment already exists");
+        }
     }
 
     public String deleteCommentById(int id) {
-        commentRepositoryImpl.updateCommentClientId(id);
-        commentRepositoryImpl.deleteById(id);
-        return "Comment has been deleted successfully for id : " + id;
+        try {
+            commentRepositoryImpl.updateCommentClientId(id);
+            commentRepositoryImpl.deleteById(id);
+            return "Comment has been deleted successfully for id : " + id;
+        } catch (Exception e) {
+            throw new NotFoundException("Comment not found for id: " + id);
+        }
     }
 
     public CommentDto updateComment(CommentDto commentDto) {
-        CommentEntity commentEntity = commentRepositoryImpl.update(commentMapper.mapToCommentEntity(commentDto));
-        return commentMapper.mapToCommentDto(commentEntity);
+        try {
+            CommentEntity commentEntity = commentRepositoryImpl.update(commentMapper.mapToCommentEntity(commentDto));
+            return commentMapper.mapToCommentDto(commentEntity);
+        } catch (Exception e) {
+            throw new NotFoundException("Comment not found for id: " + commentDto.getId());
+        }
     }
 
     public CommentDto getCommentById(int commentId) {
         CommentEntity commentEntity = commentRepositoryImpl.findById(commentId);
+        if (commentEntity == null) {
+            throw new NotFoundException("Comment not found for id: " + commentId);
+        }
         return commentMapper.mapToCommentDto(commentEntity);
     }
 }
