6ec7d12a9dbffd0e02ca429df701fda74a353095
jamesLaurino <petersthomas90@gmail.com>
2026-03-08 15:09:31 +0100

    FTO-0130 fiing cors config and path images

--- src/main/java/com/fotova/service/comment/CommentService.java
+++ src/main/java/com/fotova/service/comment/CommentService.java
@@ -1,10 +1,13 @@
 package com.fotova.service.comment;
 
+import com.fotova.dto.client.ClientCommentDto;
+import com.fotova.dto.client.ClientDto;
 import com.fotova.dto.comment.CommentDto;
 import com.fotova.entity.CommentEntity;
 import com.fotova.exception.DataExistException;
 import com.fotova.exception.NotFoundException;
 import com.fotova.repository.comment.CommentRepositoryImpl;
+import com.fotova.service.client.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +21,9 @@ public class CommentService {
 
     @Autowired
     private CommentMapper commentMapper;
+
+    @Autowired
+    private ClientService clientService;
 
     public List<CommentDto> getAllComments() {
         return commentMapper.mapToCommentDtoList(commentRepositoryImpl.findAll());
@@ -35,6 +41,25 @@ public class CommentService {
 
     public String deleteCommentById(int id) {
         try {
+            commentRepositoryImpl.updateCommentClientId(id);
+            commentRepositoryImpl.deleteById(id);
+            return "Comment has been deleted successfully for id : " + id;
+        } catch (Exception e) {
+            throw new NotFoundException("Comment not found for id: " + id);
+        }
+    }
+
+    public String deleteCommentByIdWithClientId(int id, int clientId) {
+        try {
+
+            ClientDto clientDto = clientService.getClientById(clientId);
+
+            for(ClientCommentDto clientCommentDto: clientDto.getCommentEntities()) {
+                if(clientCommentDto.getId() == id) {
+                    throw new NotFoundException("Comment not found for id: " + id);
+                }
+            }
+
             commentRepositoryImpl.updateCommentClientId(id);
             commentRepositoryImpl.deleteById(id);
             return "Comment has been deleted successfully for id : " + id;
